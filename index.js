8c645c47c17a9e4b9032b7330f76dc73c6945a7b
Vanilio1997 <Vlad.rabota.96@yandex.ru>
2023-07-20 21:01:23 +0300

    swap setinterval on createTimerAnimator

--- index.js
+++ index.js
@@ -9,40 +9,47 @@ buttonEl.disabled = true
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
+  let secondsId;
   return (seconds) => {
-    const floorTime = (Timevalue) => Math.floor(Timevalue)
-    // Округляет значение  величины времени в меньшую сторону
-    clearInterval(timerId)
-    const changeZeroValue = (value) => value < 10 ? `0${value}` : value 
-    // Если одно из значнеий времени равно 0 возвращает '00' 
-    let changigTimerSeconds = seconds
-    
-    timerId = setInterval(timerWork, 1000)
-    
-    function timerWork (){     
-      let timerHours = changeZeroValue(floorTime(changigTimerSeconds / 60 / 60))
-      let timerMinutes = changeZeroValue(floorTime((changigTimerSeconds  - (timerHours * 60 * 60)) / 60))
-      let timerSeconds = changeZeroValue(floorTime(changigTimerSeconds - (timerMinutes * 60 + timerHours * 60 * 60)))
-      
+    const endTime = new Date().getTime() + (seconds * 1000);
+    const cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
 
-      timerEl.innerHTML = `${timerHours}:${timerMinutes}:${timerSeconds}`
+    cancelAnimationFrame(secondsId)
+    function getRemainingTime(deadline) {
+      const currentTime = new Date().getTime();
+      return deadline - currentTime;
+    }
+    
+    function pad(value) {
+      return ('0' + Math.floor(value)).slice(-2);;
+    }
+    function showTime() {
+      const remainingTime = getRemainingTime(endTime);
+      const seconds = pad((remainingTime / 1000) % 60);
+      const minutes = pad((remainingTime / (60 * 1000)) % 60);
+      const hours = pad((remainingTime / (60 * 60 * 1000)) % 24);
+      timerEl.innerHTML = `${hours}:${minutes}:${seconds}`;
 
-      if(changigTimerSeconds === 0){
-        clearInterval(timerId)
-      }   
-      changigTimerSeconds--
-    }   
+
+      if (remainingTime >= 1000) {
+        secondsId =  requestAnimationFrame(showTime);
+      }
+    }
+
+    secondsId = requestAnimationFrame(showTime);
   };
-};
+  
+}
 
-console.log(inputEl.value);
 
 const animateTimer = createTimerAnimator();
 
 inputEl.addEventListener('input', (e) => {
   // Очистите input так, чтобы в значении
   // оставались только числа
-  inputEl.value =  e.target.value.replace(/[^0-9]/g, '');
+  const numbersCheck  = e.target.value.replace(/[^0-9]/g, '');
+  inputEl.value =   +numbersCheck > 0 ? numbersCheck : 1;
+
   if(inputEl.value){
     buttonEl.disabled = false
   } else {
